5545c030c97d1bab40026e03581f7e076b56adfa
Antonin <antonin.hackenberg@gmail.com>
2018-02-18 15:52:36 +0100

    Add multiple Supervisors

--- lib/pooly/worker_superviser.ex
+++ lib/pooly/worker_superviser.ex
@@ -1,25 +1,29 @@
 defmodule Pooly.WorkerSupervisor do
   use Supervisor
 
-  def start_link({_, _, _} = mfa) do
-    Supervisor.start_link(__MODULE__, mfa)
+  def start_link(pool_server, {_, _, _} = mfa) do
+    Supervisor.start_link(__MODULE__, [pool_server, mfa])
   end
 
   #############
   # Callbacks #
   #############
-  def init({m, f, a} = x) do
-    worker_opts = [ restart: :permanent, function: f ]
+  def init([pool_servrer, {m, f, a}]) do
+    Process.link(pool_servrer)
+    worker_opts = [
+      restart: :permanent,
+      shutdown: 5000,
+      function: f
+      ]
 
     children = [worker(m, a, worker_opts)]
 
     opts = [
       strategy: :simple_one_for_one,
-      max_restarts: 5,
+      max_restarts: 5, # change to 0 and kill any running process. It will kill the whole supervisor tree
       max_seconds: 5
     ]
 
     supervise(children, opts)
   end
-
 end
